e9cba10e4c4537cb80517a6d2111568fd1af3a51
ralflima <ralf.lima@gmail.com>
2023-01-19 13:10:02 -0300

    Aula 4

--- aula3/app.js
+++ aula3/app.js
@@ -10,26 +10,14 @@ const app = express();
 // Bootstrap
 app.use('/css', express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
+// Manipular informações vindas de formulário
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 // Configuração do express-handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-
-// Função para obter os Pokémons
-
-
-async function obterPokémons(){
-
-    let pokemons = [];
-
-    for(let i=1; i<=151; i++){
-        fetch('https://pokeapi.co/api/v2/pokemon/'+i)
-        .then(retorno => retorno.json())
-        .then(retorno => pokemons.push(retorno));
-    }
-
-    return pokemons;
-}
 
 // Rotas
 app.get('/', (req, res) => {
@@ -44,14 +32,54 @@ app.get('/pagina2', (req, res) => {
 
 });
 
+app.get('/exercicio1', (req, res) => {
+    res.render('exercicio1', {'exibirDados':false});
+});
+
+app.post('/exercicio1', (req, res) => {
+
+    let cep = req.body.cep;
+    fetch('https://viacep.com.br/ws/'+cep+'/json/')
+    .then(retorno => retorno.json())
+    .then(retorno => res.render('exercicio1', {'exibirDados':true, 'dados':retorno}));
+
+});
+
 app.get('/exercicio2', async (req, res) => {
 
-    let cep = '89010204';
-    fetch('https://viacep.com.br/ws/'+cep+'/json/')
-    .then(r => r.json())
-    .then(r => res.render('exercicio2', {'dados':r}));
+    // Vetor contendo os Pokémons
+    let pokemons = [];
 
-    res.end();
+    // Laço de repetição e requisição
+    for(let i=1; i<=151; i++){
+        const requisicao = await fetch('https://pokeapi.co/api/v2/pokemon/'+i);
+        const conversao = await requisicao.json()
+        pokemons.push(conversao);
+    }
+
+    console.log(pokemons);
+    
+    // Renderizar página
+    res.render('exercicio2', {'pokemons':pokemons});
+});
+
+app.get('/exercicio3', async (req, res) => {
+
+    // Objeto contendo os dados do dólar e do euro
+    let dolar = '';
+    let euro  = '';
+
+    // Requisições
+    const requisicaoDolar = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+    const converterDolar = await requisicaoDolar.json()
+    dolar = (converterDolar.USDBRL);
+
+    const requisicaoEuro = await fetch('https://economia.awesomeapi.com.br/json/last/EUR-BRL')
+    const converterEuro = await requisicaoEuro.json()
+    euro = (converterEuro.EURBRL);
+    
+    // Renderizar página
+    res.render('exercicio3', {'dolar':dolar,'euro':euro});
 });
 
 // Porta do servidor
